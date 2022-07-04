a83877d5843b8a2e79bf790875c00a11003577cf
OVECJOE <ovecjoe123@gmail.com>
2022-07-04 15:35:04 +0100

    Trying to fix error showing up on heroku page

--- config/database.js
+++ config/database.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/kiddiepad';
-
 exports.connect = () => {
   // Connecting to the database
-  mongoose
-    .connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+  mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/kiddiepad')
     .then(() => {
       console.log("Successfully connected to database");
     })
