4ce077f65699bbbe8cf0a675106a479e8eeac828
OVECJOE <ovecjoe123@gmail.com>
2022-07-04 15:24:56 +0100

    setting up to push to heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const { MONGO_URI } = process.env;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/kiddiepad';
 
 exports.connect = () => {
   // Connecting to the database
