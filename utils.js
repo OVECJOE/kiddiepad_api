45e6b8c7dd64f864c7260d5951da7cf41cb55af3
OVECJOE <ovecjoe123@gmail.com>
2022-07-05 04:51:16 +0100

    Fixed TypeError in utils.js: assignment to constant variable

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 function computeAge(dateString) {
     const today = new Date();
     const birthDate = new Date(dateString);
-    const age = today.getFullYear() - birthDate.getFullYear();
+    let age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age--;
